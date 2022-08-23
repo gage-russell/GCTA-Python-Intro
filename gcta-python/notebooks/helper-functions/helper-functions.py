08005f442ff6fc6fe9bedbae12a6424c6d517bbc
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-08-22 23:18:12 -0500

    updated helper fns for comparison operator practice

--- gcta-python/notebooks/helper-functions/helper-functions.py
+++ gcta-python/notebooks/helper-functions/helper-functions.py
@@ -1,3 +1,5 @@
+
+# identify data types ('BasicDataTypes...')
 def id_data_type(id_this: any, data_type: any) -> str | tuple:
     """
 
@@ -13,6 +15,30 @@ def id_data_type(id_this: any, data_type: any) -> str | tuple:
     exclusion_set = (' ', '\'', '\"', '\"\"\"', '<', '>')
     clean_id = ''.join([i for i in list(str(type_object).lstrip('<class ')) if i not in exclusion_set])
     return clean_id == data_type
+
+
+# List comparison operators in a string, separated by spaces ('ComparisonOperators...')
+def comparison_operator_check(operators: str) -> bool:
+    operator_list = "== != > < >= <=".split(' ')
+    # remove spaces
+    usr_inp_no_spaces = [i for i in operators.split(' ') if i != '']
+    # check for uniqueness
+    usr_inp_hashed = {}
+    for each_operator in usr_inp_no_spaces:
+        usr_inp_hashed[each_operator] = ''
+
+    if len(usr_inp_no_spaces) != 6:
+        return False
+
+    for each_operator in operator_list:
+        if each_operator not in usr_inp_no_spaces:
+            return False
+
+    return True
+
+
+
+
 
 
 print("Helper functions successfully loaded!")
